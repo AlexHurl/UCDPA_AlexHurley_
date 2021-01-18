1a74f3cebd93e217953b7e669e621e6ffe2b8a92
AlexHurl <75031882+AlexHurl@users.noreply.github.com>
2021-01-18 17:07:29 +0000

    Update

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 # Importing necessary packages
 import pandas as pd
-import matplotlib.pyplot as plt
-import datetime
-import numpy as np
+
+#import datetime
+
 birddata = pd.read_csv("bird_tracking.csv")
 
 # Print name of each bird
 bird_names = pd.unique(birddata.bird_name)
 print(bird_names)
 
+# Information about the CSV file
+birddata.info()
 
+# Import Matplotlib and Numpy
+import matplotlib.pyplot as plt
+import numpy as np
