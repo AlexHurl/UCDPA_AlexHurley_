b958c1f2a7b23d020101cf96259d5258045e0b25
AlexHurl <75031882+AlexHurl@users.noreply.github.com>
2021-01-22 00:38:12 +0000

    Update

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 # Importing necessary packages
 import pandas as pd
 
-import datetime
+
+
 
 birddata = pd.read_csv("bird_tracking.csv")
 
 # Print name of each bird
 bird_names = pd.unique(birddata.bird_name)
-print(bird_names)
+# print(bird_names)
 
 # Information about the CSV file
-birddata.info()
+# birddata.info()
+
+# print(birddata.values)
+
+# print(birddata.head())
+
+# print(birddata.index)
+
+# print(birddata.describe())
+
+# print(birddata.sort_values("speed_2d", ascending=False))
+
 
 # Import Matplotlib and Numpy
+
 import matplotlib.pyplot as plt
 import numpy as np
-
-
+import datetime
