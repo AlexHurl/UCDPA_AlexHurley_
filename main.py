720fd5fd91782f8e1bd1420ba0bb286449a0f753
AlexHurl <75031882+AlexHurl@users.noreply.github.com>
2021-01-22 20:55:38 +0000

    Update

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 # Importing necessary packages
 import pandas as pd
 
-
-
-
 birddata = pd.read_csv("bird_tracking.csv")
 
 # Print name of each bird
@@ -23,6 +20,12 @@ bird_names = pd.unique(birddata.bird_name)
 
 # print(birddata.sort_values("speed_2d", ascending=False))
 
+# Eric_MaxSpeed = birddata[(birddata["bird_name"] == "Eric") | (birddata["speed_2d"])]
+# print(Eric_MaxSpeed)
+
+# Max and Min dates
+print(birddata["date_time"].max())
+print(birddata["date_time"].min())
 
 # Import Matplotlib and Numpy
 
