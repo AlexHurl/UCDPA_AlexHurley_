4366fcd57938433b7955dc0b17f04673160409bb
AlexHurl <75031882+AlexHurl@users.noreply.github.com>
2021-01-23 01:20:08 +0000

    Update

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # Importing necessary packages
 import pandas as pd
-
+import matplotlib.pyplot as plt
+import numpy as np
+import datetime
 birddata = pd.read_csv("bird_tracking.csv")
 
 # Print name of each bird
@@ -24,11 +26,30 @@ bird_names = pd.unique(birddata.bird_name)
 # print(Eric_MaxSpeed)
 
 # Max and Min dates
-print(birddata["date_time"].max())
-print(birddata["date_time"].min())
+# print(birddata["date_time"].max())
+# print(birddata["date_time"].min())
 
-# Import Matplotlib and Numpy
+# print(birddata.drop_duplicates(subset="bird_name"))
 
-import matplotlib.pyplot as plt
-import numpy as np
-import datetime
+#
+# print(birddata["bird_name"].value_counts(sort=True))
+
+# Perecentages of data
+# print(birddata["bird_name"].value_counts(normalize=True))
+
+# Mean speed
+# print(birddata[birddata["bird_name"] == "Eric"]["speed_2d"].mean())
+# print(birddata[birddata["bird_name"] == "Sanne"]["speed_2d"].mean())
+# print(birddata[birddata["bird_name"] == "Nico"]["speed_2d"].mean())
+
+# Grouping by name with mean speed
+# print(birddata.groupby("bird_name")["speed_2d"].mean())
+
+
+#bird_stats = birddata.groupby("bird_name")["speed_2d"].agg([np.mix, np.max, np.mean, np.median])
+#print(bird_stats)
+
+
+# Grouping with agg
+# print(birddata.groupby("bird_name")["speed_2d"].agg([min, max]))
+
