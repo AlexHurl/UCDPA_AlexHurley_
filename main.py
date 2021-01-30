51124b28566a7da7932f6687ac33ee5024f88eaa
AlexHurl <75031882+AlexHurl@users.noreply.github.com>
2021-01-30 22:42:35 +0000

    Update

--- main.py
+++ main.py
@@ -156,6 +156,28 @@ print(datetime.datetime.today())
 
 
 
+data = birddata[birddata.bird_name == "Eric"]
+times = data.timestamp
+elapsed_time = [time - times[0] for time in times]
+elapsed_days = np.array(elapsed_time) / datetime.timedelta(days=1)
+
+next_day = 1
+inds = []
+daily_mean_speed = []
+for (i, t) in enumerate(elapsed_days):
+    if t < next_day:
+        inds.append(i)
+    else:
+        # compute mean speed
+        daily_mean_speed.append(np.mean(data.speed_2d[inds]))
+        next_day += 1
+        inds = []
+
+plt.figure(figsize=(8, 6))
+plt.plot(daily_mean_speed)
+plt.xlabel("Day")
+plt.ylabel("Mean speed (m/s)")
+plt.show()
 
 
 
