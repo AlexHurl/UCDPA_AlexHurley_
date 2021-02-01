6df95c772f02301cbae5d34de138249deeda1bce
AlexHurl <75031882+AlexHurl@users.noreply.github.com>
2021-02-01 02:19:21 +0000

    Update

--- main.py
+++ main.py
@@ -10,35 +10,39 @@ birddata = pd.read_csv("bird_tracking.csv")
 bird_names = pd.unique(birddata.bird_name)
 # print(bird_names)
 
-# Information about the CSV file
+# # Information about the CSV file
 # birddata.info()
-
+#
 # print(birddata.values)
-
+#
 # print(birddata.head())
-
+#
 # print(birddata.index)
-
+#
 # print(birddata.describe())
-
+#
+#
 # print(birddata.sort_values("speed_2d", ascending=False))
 
-# Eric_MaxSpeed = birddata[(birddata["bird_name"] == "Eric") | (birddata["speed_2d"])]
+# Eric_MaxSpeed = birddata[(birddata["bird_name"] == "Eric") | (birddata["speed_2d"] == max)]
 # print(Eric_MaxSpeed)
 
 # Max and Min dates
 # print(birddata["date_time"].max())
 # print(birddata["date_time"].min())
 
-# print(birddata.drop_duplicates(subset="bird_name"))
+# Drop dupilcates of speed
+#Dupes_dropped = birddata.drop_duplicates(subset="speed_2d")
+#print(Dupes_dropped)
 
-#
+
+# Value counts
 # print(birddata["bird_name"].value_counts(sort=True))
 
 # Perecentages of data
 # print(birddata["bird_name"].value_counts(normalize=True))
 
-# Mean speed
+# # Mean speed
 # print(birddata[birddata["bird_name"] == "Eric"]["speed_2d"].mean())
 # print(birddata[birddata["bird_name"] == "Sanne"]["speed_2d"].mean())
 # print(birddata[birddata["bird_name"] == "Nico"]["speed_2d"].mean())
@@ -47,22 +51,25 @@ bird_names = pd.unique(birddata.bird_name)
 # print(birddata.groupby("bird_name")["speed_2d"].mean())
 
 # Aggregating data (Multiple grouped summaries
-# bird_stats = birddata.groupby("bird_name")["speed_2d"].agg([np.mix, np.max, np.mean, np.median])
+# bird_stats = birddata.groupby("bird_name")("speed_2d")agg([np.mix, np.max, np.mean, np.median])
 # print(bird_stats)
 
 
-# Grouping with agg
+
+
+# Grouping with agg stats on max and min for each bird
 # print(birddata.groupby("bird_name")["speed_2d"].agg([min, max]))
+#
+# print(birddata.groupby("bird_name")["altitude"].agg([min, max]))
+#
+# print(birddata.groupby("bird_name")["longitude"].agg([min, max]))
+#
+# print(birddata.groupby("bird_name")["latitude"].agg([min, max]))
 
-# Indexing
-# birddata_2 = birddata.set_index("bird_name")
-# print(birddata_2)
 
-# print(birddata_2.loc[["Eric", "Sanne"]])
-
-# Multi Level index
-# birddata_index = birddata.set_index(["bird_name", "altitude"]).sort_index()
-# birddata_sorted = ["Eric", "Sanne", "Nico"]
+#Multi Level index .Here we can sort the bird data using  MLI so we can each bird from the persepective of how how long they were travelling for using the bird_name and date_time
+# birddata_index = birddata.set_index(["bird_name", "date_time"]).sort_index()
+# birddata_sorted = ["Sanne"]
 # print(birddata_index.loc[birddata_sorted])
 
 # subsetting by date
@@ -76,109 +83,121 @@ bird_names = pd.unique(birddata.bird_name)
 
 
 
-# Plot latitude and longitude
-# # ix = birddata.bird_name == "Nico"
+# Plot latitude and longitude of Eric Nice and Sanne
+# ix = birddata.bird_name == "Eric"
 #
-# # x, y = birddata.longitude[ix], birddata.latitude[ix]
+# x, y = birddata.longitude[ix], birddata.latitude[ix]
 #
-# # plt.figure(figsize=(7,7))
-# # plt.plot(x,y,".")
+# plt.figure(figsize=(7,7))
+# plt.plot(x,y,".")
 #
-# # bird_names = pd.unique(birddata.bird_name)
-#
-# # bird_names = pd.unique(birddata.bird_name)
-# # plt.figure(figsize=(7,7))
-#  # for bird_name in bird_names:
-#   #  ix = birddata.bird_name == bird_name
-#   #  x, y = birddata.longitude[ix], birddata.latitude[ix]
-#
-#  #   plt.plot(x, y, ".", label=bird_name)
-# # plt.xlabel("Birds Longitude")
-# # plt.ylabel("Birds Latitude")
-# # plt.legend(loc="lower right")
+# bird_names = pd.unique(birddata.bird_name)
+
+  ## store the unique bird name as vairable x and y.
+# bird_names = pd.unique(birddata.bird_name)
+# plt.figure(figsize=(7,7))
+
+ ## Use X and Y labels for longitude and latitude respectively
+# for bird_name in bird_names:
+#   ix = birddata.bird_name == bird_name
+#   x, y = birddata.longitude[ix], birddata.latitude[ix]
+#   plt.plot(x, y, ".", label=bird_name)
+# plt.xlabel("Birds Longitude")
+# plt.ylabel("Birds Latitude")
+# plt.legend(loc="lower right")
 # plt.show()
+
 
 
 
 
 # Calculating mean speed
 
-datetime.datetime.today()
+# datetime.datetime.today()
+#
+# time_1 = datetime.datetime.today()
+#
+# time_2 = datetime.datetime.today()
+#
+# time_2 - time_1
+#
+# time_2 = datetime.datetime.today()
+#
+# time_2 - time_1
+#
+# date_str = birddata.date_time[0]
+#
+# date_str[:-3]
+#
+# datetime.datetime.strptime(date_str[:-3], "%Y-%m-%d %H:%M:%S")
+#
+# timestamps = []
+# for k in range(len(birddata)):
+#     timestamps.append(datetime.datetime.strptime\
+#     # Slices the +00 from the time stamps
+#     (birddata.date_time.iloc[k][:-3], "%Y-%m-%d %H:%M:%S"))
+#
+# timestamps[0:3]
+#
+# birddata["timestamp"] = pd.Series(timestamps, index=birddata.index)
+#
+# birddata.head()
+#
+# birddata.timestamp[4] - birddata.timestamp[3]
 
-time_1 = datetime.datetime.today()
+# ix = birddata.bird_name == "Eric"
+#
+# speed = birddata.speed_2d[ix]
 
-time_2 = datetime.datetime.today()
+# seaborn to show frequency of speeds
+# birddata["speed_2d"].plot.hist()
+# plt.show()
 
-time_2 - time_1
-
-time_2 = datetime.datetime.today()
-
-time_2 - time_1
-
-date_str = birddata.date_time[0]
-
-date_str[:-3]
-
-datetime.datetime.strptime(date_str[:-3], "%Y-%m-%d %H:%M:%S")
-
-timestamps = []
-for k in range(len(birddata)):
-    timestamps.append(datetime.datetime.strptime\
-    # Slices the +00 from the time stamps
-    (birddata.date_time.iloc[k][:-3], "%Y-%m-%d %H:%M:%S"))
-
-timestamps[0:3]
-
-birddata["timestamp"] = pd.Series(timestamps, index=birddata.index)
-
-birddata.head()
-
-birddata.timestamp[4] - birddata.timestamp[3]
 
 #Make a list that captures the amount fo time since the start of the data collection
-times = birddata.timestamp[birddata.bird_name == "Eric"]
-elapsed_time = [time - times[0] for time in times]
-
-elapsed_time[1000]
-
-elapsed_time[1000] / datetime.timedelta(days=1)
-
-elapsed_time[1000] / datetime.timedelta(hours=1)
-
-
-
-
-#Length of entries
-print(len(elapsed_time))
-# date time today
-print(datetime.datetime.today())
-
-
-
-
-data = birddata[birddata.bird_name == "Eric"]
-times = data.timestamp
-elapsed_time = [time - times[0] for time in times]
-elapsed_days = np.array(elapsed_time) / datetime.timedelta(days=1)
-
-next_day = 1
-inds = []
-daily_mean_speed = []
-for (i, t) in enumerate(elapsed_days):
-    if t < next_day:
-        inds.append(i)
-    else:
-        # compute mean speed
-        daily_mean_speed.append(np.mean(data.speed_2d[inds]))
-        next_day += 1
-        inds = []
-
-plt.figure(figsize=(8, 6))
-plt.plot(daily_mean_speed)
-plt.xlabel("Day")
-plt.ylabel("Mean speed (m/s)")
-plt.show()
-
+# times = birddata.timestamp[birddata.bird_name == "Eric"]
+# elapsed_time = [time - times[0] for time in times]
+#
+# elapsed_time[1000]
+#
+# elapsed_time[1000] / datetime.timedelta(days=1)
+#
+# elapsed_time[1000] / datetime.timedelta(hours=1)
+#
+#
+#
+#
+# #Length of entries
+# print(len(elapsed_time))
+# # date time today
+# print(datetime.datetime.today())
+#
+#
+#
+#
+# data = birddata[birddata.bird_name == "Eric"]
+# times = data.timestamp
+# elapsed_time = [time - times[0] for time in times]
+# elapsed_days = np.array(elapsed_time) / datetime.timedelta(days=1)
+#
+# next_day = 1
+# inds = []
+# daily_mean_speed = []
+# for (i, t) in enumerate(elapsed_days):
+#     if t < next_day:
+#         inds.append(i)
+#     else:
+#         # compute mean speed
+#         daily_mean_speed.append(np.mean(data.speed_2d[inds]))
+#         next_day += 1
+#         inds = []
+#
+# plt.figure(figsize=(8, 6))
+# plt.plot(daily_mean_speed)
+# plt.xlabel("Day")
+# plt.ylabel("Mean speed (m/s)")
+# plt.show()
+#
 
 
 
